83316305bf69fcc29c8652a486303189a373145e
Girlian Santos <girliansantos@si.fiponline.edu.br>
2022-12-05 14:41:27 -0200

    Inicializando projeto

---
+++ src/main/java/com/codeline/gameengine/core/Game.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.codeline.gameengine.core;
+
+/**
+ *
+ * @author Girlian Santos
+ */
+
+/**
+ * Classe principal, núcleo do jogo
+ * controla todo o loop
+ */
+public class Game {
+    protected int estado;
+}
